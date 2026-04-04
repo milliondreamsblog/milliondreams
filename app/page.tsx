933d48b986d1caa333949f5db8884b7b796f319e
milliondreamsblog <114696765+milliondreamsblog@users.noreply.github.com>
2026-04-04 15:58:51 +0530

    name upadte:

--- app/page.tsx
+++ app/page.tsx
@@ -477,7 +477,7 @@ export default function Home() {
                     </a>
                   </div>
                   <p className="text-sm leading-relaxed text-gray-600 dark:text-gray-400">
-                    I&apos;ve had the privilege to work with Aditya on several highly technical cybersecurity R&D projects involving design and implementation of defensive network components in Golang, network protocol research and analysis. He is a bright young engineer, extremely talented in hacking and cybersecurity, with a natural curiosity and passion for hacking, and a gift understanding how systems work, how to design and break them. I am certain that he will succeed in any endeavor he puts his mind to, in the realms of cybersecurity, engineering and beyond! :)
+                    I&apos;ve had the privilege to work with Akshaton several highly technical cybersecurity R&D projects involving design and implementation of defensive network components in Golang, network protocol research and analysis. He is a bright young engineer, extremely talented in hacking and cybersecurity, with a natural curiosity and passion for hacking, and a gift understanding how systems work, how to design and break them. I am certain that he will succeed in any endeavor he puts his mind to, in the realms of cybersecurity, engineering and beyond! :)
                   </p>
                 </div>
 
@@ -496,7 +496,7 @@ export default function Home() {
                   <p className="text-sm leading-relaxed text-gray-600 dark:text-gray-400">
                     It&apos;s not often that you get to talk to a person who is not only hungry for mentorship, but comes out of the gate with the attitude that enables him to learn so, so quickly on his feet.
                     <br /><br />
-                    Aditya did research for highly technical content for me and independently navigated difficult situations without a lot of guidance. If you&apos;re looking for someone to research a technical topic for your content work, Aditya is disciplined, thorough and insistent on understanding things in depth before giving a final output.
+                    Akshatdid research for highly technical content for me and independently navigated difficult situations without a lot of guidance. If you&apos;re looking for someone to research a technical topic for your content work, Akshatis disciplined, thorough and insistent on understanding things in depth before giving a final output.
                     <br /><br />
                     Keep on keeping on brother!
                   </p>
