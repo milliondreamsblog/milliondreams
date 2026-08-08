ee86be47976c336b65ac92c53617be2832234d54
milliondreamsblog <114696765+milliondreamsblog@users.noreply.github.com>
2026-08-08 20:17:20 +0530

    feat(evolvesanga): publish EvolveSanga case study

--- app/data/case-studies/index.ts
+++ app/data/case-studies/index.ts
@@ -4,6 +4,7 @@ import { cadence } from "./cadence";
 import { bawarchie } from "./bawarchie";
 import { roborumble } from "./roborumble";
 import { talk2pdf } from "./talk2pdf";
+import { evolvesanga } from "./evolvesanga";
 
 /**
  * Registry of case studies, keyed by Project.id. Add a new case study by
@@ -15,6 +16,7 @@ export const CASE_STUDIES: Record<string, CaseStudy> = {
   bawarchie,
   roborumble,
   talk2pdf,
+  evolvesanga,
 };
 
 export function getCaseStudy(slug: string): CaseStudy | undefined {
