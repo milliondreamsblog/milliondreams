dca1dbd0d36f96ca100a0fddfa648a3c5441f73d
milliondreamsblog <114696765+milliondreamsblog@users.noreply.github.com>
2026-08-10 22:35:10 +0530

    chore(layout): mount Footer beneath every page

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,7 @@ export default function RootLayout({
             <KonamiEasterEgg />
             <SmoothScroll>
               {children}
+              <Footer />
             </SmoothScroll>
             <Navbar />
           </NavProvider>
