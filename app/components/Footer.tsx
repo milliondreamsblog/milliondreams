f31b435dafb7dd04fd33f30d5a1a03e2e59e0f28
milliondreamsblog <114696765+milliondreamsblog@users.noreply.github.com>
2026-08-10 22:35:07 +0530

    feat(footer): scaffold footer with brand mark and name

---
+++ app/components/Footer.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Link from "next/link";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+
+
+
+
+
+
+
+
+
+export function Footer() {
+
+  return (
+    <footer className="border-t border-gray-200 bg-white px-6 pb-32 pt-14 dark:border-white/10 dark:bg-black sm:px-8 lg:px-10">
+      <div className="mx-auto flex max-w-6xl flex-col gap-12 lg:flex-row lg:items-start lg:justify-between">
+        {/* Brand + big invite */}
+        <div className="flex flex-col justify-between gap-10 lg:min-h-[240px]">
+          <Link href="/" className="flex w-fit items-center gap-2">
+            <span className="grid grid-cols-2 gap-[2px]">
+              <span className="h-[7px] w-[10px] rounded-[2px] bg-black dark:bg-white" />
+              <span className="h-[7px] w-[5px] rounded-[2px] bg-black dark:bg-white" />
+              <span className="h-[7px] w-[5px] rounded-[2px] bg-black dark:bg-white" />
+              <span className="h-[7px] w-[10px] rounded-[2px] bg-black dark:bg-white" />
+            </span>
+            <span className="text-xl font-semibold text-black dark:text-white">
+              Akshat Darshi
+            </span>
+          </Link>
+        </div>
+
+      </div>
+
+    </footer>
+  );
+}
